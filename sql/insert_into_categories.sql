a163e2444c3ddafe671fd8420387e1b09fd459b2
Michal-S-2004 <strugacz04@gmail.com>
2026-07-26 17:06:16 +0200

    inserts records into categories table

---
+++ sql/insert_into_categories.sql
@@ -0,0 +1,17 @@
+INSERT INTO categories (category_name)
+VALUES
+('Laptops'),
+('Desktop PCs'),
+('PC Components'),
+('Smartphones'),
+('Tablets'),
+('Monitors'),
+('Keyboards'),
+('Mice'),
+('Headphones'),
+('Storage Devices'),
+('Networking'),
+('Gaming'),
+('TV & Home Entertainment'),
+('Office Equipment'),
+('Accessories');
