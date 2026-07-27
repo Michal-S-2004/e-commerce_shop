4e282ecc3977fb79ada497454a78a8fc572b1373
Michal-S-2004 <strugacz04@gmail.com>
2026-07-27 20:41:39 +0200

    Refactor data generation to include customer creation
    Added functions to create employees and customers with random data.

--- python/data_generator.py
+++ python/data_generator.py
@@ -3,6 +3,7 @@ from dotenv import load_dotenv
 from unidecode import unidecode
 import psycopg
 import os
+from datetime import datetime
 
 load_dotenv()
 fake = Faker("pl_PL")
@@ -19,29 +20,58 @@ cursor = connection.cursor()
 
 cursor.execute("SELECT version();")
 print(cursor.fetchone()[0])
+# EMPLOYEES
 
-for i in range(10):
+def create_employees():
 
-    first_name = unidecode(fake.first_name())
-    last_name = unidecode(fake.last_name())
-    salary = fake.random_int(min=4000, max=12000)
-    phone_number = fake.phone_number()
-    email = f"{first_name.lower()}.{last_name.lower()}{fake.random_int(min=100, max=999)}@store.com"
+    for i in range(10):
 
-    print(first_name, last_name, salary, phone_number, email)
+        first_name = unidecode(fake.first_name())
+        last_name = unidecode(fake.last_name())
+        salary = fake.random_int(min=4000, max=12000)
+        phone_number = fake.phone_number()
+        email = f"{first_name.lower()}.{last_name.lower()}{fake.random_int(min=100, max=999)}@store.com"
 
-    cursor.execute(
-        """
-        INSERT INTO employees
-        (first_name, last_name, salary, phone_number, email)
-        VALUES (%s, %s, %s, %s, %s)
-        """,
-        (first_name, last_name, salary, phone_number, email)
-    )
+        print(first_name, last_name, salary, phone_number, email)
 
-connection.commit()
+        cursor.execute(
+            """
+            INSERT INTO employees
+            (first_name, last_name, salary, phone_number, email)
+            VALUES (%s, %s, %s, %s, %s)
+            """,
+            (first_name, last_name, salary, phone_number, email)
+        )
+def create_customers():
+    for i in range(500):
+        first_name = unidecode(fake.first_name())
+        last_name = unidecode(fake.last_name())
+        phone_number = fake.phone_number()
+        email = f"{first_name.lower()}.{last_name.lower()}{fake.random_int(min=100, max=999)}@example.com"
+        created_at = fake.date_time_between(start_date=datetime(2025, 1, 1),end_date="now")
 
-cursor.close()
-connection.close()
+        cursor.execute(
+            """
+            INSERT INTO customers
+            (first_name, last_name, phone, email, created_at)
+            VALUES (%s, %s, %s, %s, %s)
+            """,
+            (first_name, last_name, phone_number, email, created_at))
+cursor.execute("""
+UPDATE employees
+SET phone_number = REPLACE(phone_number, '+48 ', '');
+""")
 
-print("10 employees inserted successfully.")
+
+
+
+if __name__ == "__main__":
+    #create_customers()
+
+    connection.commit()
+
+    cursor.close()
+    connection.close()
+
+    print("Customers inserted successfully.")
+
