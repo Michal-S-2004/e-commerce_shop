1158480788543befa8ab4cb077536c7e6aaea7f3
Michal-S-2004 <strugacz04@gmail.com>
2026-08-20 13:54:44 +0200

    Add various SQL window function examples

--- sql/window_functions_learning.sql
+++ sql/window_functions_learning.sql
@@ -24,3 +24,28 @@ select c_o.order_id, c_o.order_date, c_o.order_price, lag(c_o.order_price)  over
 
 select c_o.order_id, c_o.order_date, c_o.order_price, lag(c_o.order_price)  over(order by c_o.order_date) as previous_price, round((c_o.order_price - lag(c_o.order_price) over(order by c_o.order_date)),2) as difference 
 from customer_orders as c_o
+
+select c_o.order_id, c_o.order_price, sum(c_o.order_price) over(order by order_date, order_id) from customer_orders as c_o
+
+select c_o.order_id, c_o.employee_id, c_o.order_price, sum(c_o.order_price) over(partition by employee_id order by order_date, order_id) from customer_orders as c_o
+
+select c_o.order_id, c_o.order_price, c_o.order_price/(sum(c_o.order_price) over(order by order_id)) as percentage from customer_orders as c_o
+
+select c_o.order_id, c_o.order_price, 100*c_o.order_price/sum(c_o.order_price)  over() as percentage from customer_orders as c_o
+
+select c_o.order_id, c_o.order_date, c_o.order_price, (avg(c_o.order_price) over(order by c_o.order_date rows between 2 preceding and current row))as moving_average from customer_orders as c_o
+
+select c_o.order_id, c_o.order_date, c_o.order_price,c_o.employee_id, round((avg(c_o.order_price) over(partition by c_o.employee_id order by c_o.order_date rows between 2 preceding and current row )),2) 
+as moving_average  from customer_orders as c_o
+
+select c_o.order_id, c_o.order_date, c_o.order_price, sum(c_o.order_price) over(order by order_date, order_id) from customer_orders as c_o
+
+
+select c_o.order_id, c_o.order_date, c_o.order_price, avg(c_o.order_price) over(order by order_date, order_id rows between 4 preceding and current row ) as moving_average,
+case when c_o.order_price>avg(c_o.order_price) over(order by order_date, order_id rows between 4 preceding and current row ) then 'above average' when c_o.order_price = avg(c_o.order_price) over(order by order_date, order_id rows between 4 preceding and current row ) then 'equals average' else 'below average' end as price_category
+from customer_orders as c_o
+
+select c_o.order_id, c_o.order_price,c_o.order_price - lead(c_o.order_price,2) over(order by c_o.order_date) as change, ntile(6) over(order by c_o.order_price) as groups from customer_orders as c_o
+
+select c_o.order_id, sum(c_o.order_price) over(order by c_o.order_date, c_o.order_id), rank() over(order by c_o.order_date, c_o.order_id) from customer_orders as c_o
+
